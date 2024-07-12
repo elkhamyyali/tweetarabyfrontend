f1a829b63cfb5becba463c48a7dfb2c4e4c1f67c
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-07-12 03:27:55 +0300

    Your commit message

--- components/Media/index.tsx
+++ components/Media/index.tsx
@@ -2,18 +2,7 @@
 import { Button, Input } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
-import { DotsIcon } from "@/components/icons/accounts/dots-icon";
-import { ExportIcon } from "@/components/icons/accounts/export-icon";
-import { InfoIcon } from "@/components/icons/accounts/info-icon";
-import { TrashIcon } from "@/components/icons/accounts/trash-icon";
-import { HouseIcon } from "@/components/icons/breadcrumb/house-icon";
-import { UsersIcon } from "@/components/icons/breadcrumb/users-icon";
-import { SettingsIcon } from "@/components/icons/sidebar/settings-icon";
-import { TableWrapper } from "@/components/table/table";
-import { Tablebots } from "@/components/botstable/Tablebots";
-import TableNew from "../tweets/campaigns/TableNew";
-import TableNewTwo from "../tweets/sessions/TableNew";
-import TableSessions from "../tweets/sessionhandler/TableSessions";
+
 import TableWithCheckbox from "../medias/mediafolder/FirstMedia";
 import CheckboxTable from "../medias/niches/NichesTable";
 import TweetTable from "../medias/tweets/TweetsTable";
