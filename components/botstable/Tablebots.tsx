c4981dda398cf1c1043072293d4deb332c583462
Omar Hatem <77906692+Wolvortex@users.noreply.github.com>
2024-07-13 04:11:51 +0300

    removed () from option

--- components/botstable/Tablebots.tsx
+++ components/botstable/Tablebots.tsx
@@ -248,7 +248,7 @@ const Tablebots: React.FC = () => {
                     <SelectItem key={""} value="">
                       Select {column.name}
                     </SelectItem>
-                    {column.filterOptions.map((option) => (
+                    {column.filterOptions.map(option => (
                       <SelectItem key={option} value={option}>
                         {option}
                       </SelectItem>
