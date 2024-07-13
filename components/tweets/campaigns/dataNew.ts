c373f32dc746f8162f99f7e7a507ada880f61660
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-07-13 11:49:27 +0300

    Your commit

--- components/tweets/campaigns/dataNew.ts
+++ components/tweets/campaigns/dataNew.ts
@@ -1,29 +1,100 @@
-// dataNew.ts
-export const initialData: any = [
+
+
+
+
+export const initialData : any = [
   {
     id: 1,
-    campaignName: "Campaign 1",
-    sessionNiche: "Premium",
-    burnerBots: "Top",
-    isActive: true,
+    campaign: "Campaign 1",
     mainType: "Premium",
     subType: "Top",
     limitedRepeatance: true,
     inSession: "Session 1",
+    sessionHandler: "Handler 1",
   },
   {
     id: 2,
-    campaignName: "Campaign 2",
-    sessionNiche: "Burn",
-    burnerBots: "Latest",
-    isActive: false,
+    campaign: "Campaign 2",
     mainType: "Burn",
     subType: "Latest",
     limitedRepeatance: false,
     inSession: "Session 2",
+    sessionHandler: "Handler 2",
   },
-  // Add more objects as needed
+  {
+    id: 3,
+    campaign: "Campaign 3",
+    mainType: "Premium",
+    subType: "Top",
+    limitedRepeatance: false,
+    inSession: "Session 3",
+    sessionHandler: "Handler 3",
+  },
+  {
+    id: 4,
+    campaign: "Campaign 4",
+    mainType: "Burn",
+    subType: "Latest",
+    limitedRepeatance: true,
+    inSession: "Session 4",
+    sessionHandler: "Handler 4",
+  },
+  {
+    id: 5,
+    campaign: "Campaign 5",
+    mainType: "Premium",
+    subType: "Top",
+    limitedRepeatance: true,
+    inSession: "Session 5",
+    sessionHandler: "Handler 5",
+  },
+  {
+    id: 6,
+    campaign: "Campaign 6",
+    mainType: "Burn",
+    subType: "Top",
+    limitedRepeatance: false,
+    inSession: "Session 6",
+    sessionHandler: "Handler 6",
+  },
+  {
+    id: 7,
+    campaign: "Campaign 7",
+    mainType: "Premium",
+    subType: "Latest",
+    limitedRepeatance: true,
+    inSession: "Session 7",
+    sessionHandler: "Handler 7",
+  },
+  {
+    id: 8,
+    campaign: "Campaign 8",
+    mainType: "Burn",
+    subType: "Top",
+    limitedRepeatance: false,
+    inSession: "Session 8",
+    sessionHandler: "Handler 8",
+  },
+  {
+    id: 9,
+    campaign: "Campaign 9",
+    mainType: "Premium",
+    subType: "Top",
+    limitedRepeatance: true,
+    inSession: "Session 9",
+    sessionHandler: "Handler 9",
+  },
+  {
+    id: 10,
+    campaign: "Campaign 10",
+    mainType: "Burn",
+    subType: "Latest",
+    limitedRepeatance: false,
+    inSession: "Session 10",
+    sessionHandler: "Handler 10",
+  },
 ];
+
 
 export const columns = [
   { name: "ID", uid: "id" },
